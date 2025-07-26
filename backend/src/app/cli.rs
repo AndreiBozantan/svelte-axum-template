d69880013588d04d58aa0b1b6fc9937d28c48668
Andrei Bozantan <andrei.bozantan@gmail.com>
2025-07-26 18:17:47 +0200

    fix cli

--- backend/src/app/cli.rs
+++ backend/src/app/cli.rs
@@ -170,9 +170,9 @@ async fn migrate_action_create_admin(
     let password_hash = auth::hash_password(&password)
         .map_err(|_| CliError::Other("Failed to hash password".to_string()))?;
     let new_user = db::NewUser {
-        username: username,
+        username,
         password_hash: Some(password_hash),
-        email: email,
+        email,
         tenant_id: Some(1), // Default tenant
         sso_provider: None,
         sso_id: None,
