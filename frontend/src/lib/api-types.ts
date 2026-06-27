eed6504c1672e5ceb3928e5c6d00eb8935cfd163
Andrei Bozantan <andrei.bozantan@gmail.com>
2026-06-27 14:08:28 +0000

    introduce type shims

---
+++ frontend/src/lib/api-types.ts
@@ -0,0 +1,5 @@
+import type { components } from './generated/api';
+
+export type User = components['schemas']['UserResponse'];
+export type LoginResponse = components['schemas']['LoginResponse'];
+export type ApiErrorResponse = components['schemas']['ApiError'];
